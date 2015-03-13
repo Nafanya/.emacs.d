7a1249a9a393318c4b5e36d5d616d913a6e2306f
Nikita Yaschenko <nikita.yaschenko@gmail.com>
2015-03-14 01:00:55 +0300

    Update init.el Change default editor font.

--- init.el
+++ init.el
@@ -28,3 +28,15 @@
 (ido-vertical-mode 1)
 (ido-ubiquitous-mode 1)
 (flx-ido-mode 1)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Fantasque Sans Mono" :foundry "unknown" :slant normal :weight normal :height 128 :width normal)))))
